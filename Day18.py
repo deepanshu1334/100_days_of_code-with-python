9b90329716afbf8ac11d6af8c10f6949ccb91105
deepanshu1334 <106024910+deepanshu1334@users.noreply.github.com>
2022-09-03 23:54:25 +0530

    Add files via upload

---
+++ Day18.py
@@ -0,0 +1,28 @@
+for e in range(1,10,1):
+    print(e)
+print()    
+
+r2=range(1,21,1)
+#print(r1)
+print()
+for e in r2:
+    print(e)
+print()
+
+print(r2.count(1))
+print()
+print(r2.index(2))
+print()
+
+#3
+
+for e in range(1,21,-1):
+    print(e)
+print()    
+
+#4
+
+s1="deepanshu kumar champion"
+print(s1.endswith("ion"))
+print(s1[3])
+print()
