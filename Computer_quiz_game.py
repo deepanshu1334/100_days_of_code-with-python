43261f6219c8f00d4dafacb58ef8fe5076e10e07
deepanshu1334 <106024910+deepanshu1334@users.noreply.github.com>
2022-09-05 00:03:40 +0530

    Add files via upload

---
+++ Computer_quiz_game.py
@@ -0,0 +1,13 @@
+print("welcome to computer quiz")
+playing=input()
+
+if playing!="yes":
+    quit()
+
+print("lets playthe the game")
+
+answer=input("what does cpu stand for? ")
+if answer =="central processing unit":
+    print("correct")
+else:
+    print("incorrect")    
